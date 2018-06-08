dde5e0303bf418c6cbcbfbee1fd6128f0b915123
Ragan Walker <rwalker@contractor.lb.com>
2018-06-08 17:20:58 -0400

    add blog structure

--- UserNotificationTestApp/MyPlayground.playground/Contents.swift
+++ UserNotificationTestApp/MyPlayground.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+/*:
+ # Outline
 
-var str = "Hello, playground"
+ ## Blog 1 - 3 Updates to User Notification Management
+    * Provisional & Critical Notifications
+    * Grouped NOtifications
+    * Custom Notification Settings
+
+ _Notes:_ Grouped notifications need to inlcude summary strings, and localiztaion
+
+ ## Blog 2 - Interactive User Notifications in iOS12
+    * What's New in ContentExtension API
+    * Examples of building interactive notification:
+        1. Banking transfer
+        2. Retail purchase
+        3. General Design recommendations
+    * Business Value
+    * Consideration
+
+ */

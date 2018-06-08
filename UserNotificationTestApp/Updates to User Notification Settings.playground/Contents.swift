4ae1635125c0912ac025bdeeecda260904a9ba23
Ragan Walker <rwalker@contractor.lb.com>
2018-06-08 17:29:43 -0400

    add separate playgrounds for each blog

---
+++ UserNotificationTestApp/Updates to User Notification Settings.playground/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ # 3 Updates to User Notification Management
+ 
+ ## 1. Provisional & Critical Notifications
+ 
+ ## 2. Grouped Notifications
+  _Notes:_ Grouped notifications need to inlcude summary strings, and localiztaion
+ 
+ ## 3. Custom Notification Settings
+
+ */

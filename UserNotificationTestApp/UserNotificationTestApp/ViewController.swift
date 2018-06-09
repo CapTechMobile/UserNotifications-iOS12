2391649aa8db056f2c5c0b62d396f44433dec1a5
Ragan Walker <rwalker@contractor.lb.com>
2018-06-09 11:36:35 -0400

    remove playgrounds and add a few lines of samply code

--- UserNotificationTestApp/UserNotificationTestApp/ViewController.swift
+++ UserNotificationTestApp/UserNotificationTestApp/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func sendLocalNotificationWithAttachment(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        
+        // Set title and subtitle.
+        content.title = "ALERT"
+        content.body = "Incoming notification coming your way. This is important."
+        content.sound = UNNotificationSound.defaultCritical
+        
+        // Attach an image.
+        if let fileURL = Bundle.main.url(forResource:"cat", withExtension: "jpg"),
+            let attachment = try? UNNotificationAttachment(identifier: "localAttachment", url: fileURL, options: nil) {
+            content.attachments = [attachment]
+        }
+        content.categoryIdentifier = "testCategory"
+        
+        // Send notification after 5 seconds.
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        
+        // Set up notification request.
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // Schedule notification request.
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                fatalError("failed to schedule notification request: \(request) with \(error)")
+            }
+        }
     }
-
 
 }
 

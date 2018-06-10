a4d9a754081fc591a8546f7f0ca7d15b46c0ceaa
Ragan Walker <rwalker@contractor.lb.com>
2018-06-10 10:12:39 -0400

    add edit transfer rich notification

---
+++ UserNotificationTestApp/UserNotificationTestApp/Banking/EditTransferViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditTransferViewController.swift
+//  UserNotificationTestApp
+//
+//  Created by Ragan Walker on 6/10/18.
+//  Copyright © 2018 SillyRags. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class EditTransferViewController: UIViewController, UNNotificationContentExtension {
+    
+    func didReceive(_ notification: UNNotification) {
+        
+    }
+    
+}
